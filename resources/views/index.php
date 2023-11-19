58b6e6ece9a678c1a9d4dca20a12394c784ea5a7
Noor30903 <nolalwlaqi@effat.edu.sa>
2023-11-19 16:43:06 +0300

    we are all here
    comment we are all here

--- resources/views/index.php
+++ resources/views/index.php
@@ -1,11 +1,8 @@
 <?php
 //noor added this file to work on main page.
-<<<<<<< Updated upstream
-=======
 //maha was here
 //mashael was here
 // Walaa was here
 //we are all here
->>>>>>> Stashed changes
 
 ?>
