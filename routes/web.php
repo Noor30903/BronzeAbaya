d015c573aa4fdcd25ac9efd18afff31e20632c35
Maha182 <maaalmashharawi@effat.edu.sa>
2023-11-23 17:49:51 +0300

    Creat AdminMiddleware file

--- routes/web.php
+++ routes/web.php
@@ -21,13 +21,20 @@ Route::get('admin/logout', [AuthControllersiclass::class, 'logout_admin']);
 /*mashael*/
 
 
+Route::group (['middleware' => 'admin'], function () {
+    Route::get('admin/dashboard', function () {
+    return view('admin.dashboard');
+    });
+    Route::get('admin/admin/list', function () {
+    return view('admin.admin.list');
+    });
+});
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('admin/dashboard', function () {
-    return view('admin.dashboard');
-});
+
     
     
