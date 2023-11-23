0a65b12a52c6533416b58ce78e929f659295a747
Noor30903 <nolalwlaqi@effat.edu.sa>
2023-11-23 22:23:14 +0300

    updating the add file
    just made the inputs required

--- resources/views/admin/admin/add.blade.php
+++ resources/views/admin/admin/add.blade.php
@@ -30,19 +30,19 @@
                 <div class="card-body">
                   <div class="form-group">
                     <label>Name</label>
-                    <input type="text" class="form-control" name="name" placeholder="Enter Name">
+                    <input type="text" class="form-control" required name="name" placeholder="Enter Name">
                   </div>
                   <div class="form-group">
                     <label>Email address</label>
-                    <input type="email" class="form-control" name="email" placeholder="Enter Email">
+                    <input type="email" class="form-control" required name="email" placeholder="Enter Email">
                   </div>
                   <div class="form-group">
                     <label >Password</label>
-                    <input type="password" class="form-control" name="password" placeholder="Password">
+                    <input type="password" class="form-control" required name="password" placeholder="Password">
                   </div> 
                   <div class="form-group">
                     <label >Status</label>
-                    <select class="form-control" name="status">
+                    <select class="form-control" required name="status">
                         <option value="0">Active</option>
                         <option value="1">Inactive</option>
                     </select>
