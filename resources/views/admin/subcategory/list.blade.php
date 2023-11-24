6f845868650dd00edd94521d5666af018536be43
Noor30903 <nolalwlaqi@effat.edu.sa>
2023-11-24 22:06:28 +0300

    creating the subcategory controller

--- resources/views/admin/subcategory/list.blade.php
+++ resources/views/admin/subcategory/list.blade.php
@@ -72,6 +72,9 @@
 
                   </tbody>
                 </table>
+                <div style="padding:10px; float:right;">
+                    {!! $getRecord->appends(Illuminate\Support\Facades\Request::except('page'))->links()!!}
+                </div>
               </div>
               <!-- /.card-body -->
             </div>
