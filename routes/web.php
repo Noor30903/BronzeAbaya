05a59ef1ac0d01b82f8d21ba681a10fe4bd0e7cf
Noor30903 <nolalwlaqi@effat.edu.sa>
2023-11-24 17:39:58 +0300

    creating the Category controller
    create the controller for the categories and adding routes in the web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,15 +28,20 @@ Route::get('admin/logout', [AuthController::class, 'logout_admin']);
 Route::group (['middleware' => 'admin'], function () {
 
     Route::get('admin/dashboard', [DashboardController::class, 'dashboard']);
+
     Route::get('admin/admin/list', [AdminController::class, 'list']);
     Route::get('admin/admin/add', [AdminController::class, 'add']);
     Route::post('admin/admin/add', [AdminController::class, 'insert']);
-
     Route::get('admin/admin/edit/{id}', [AdminController::class, 'edit']);
     Route::post('admin/admin/edit/{id}', [AdminController::class, 'update']);
     Route::get('admin/admin/delete/{id}', [AdminController::class, 'delete']);
 
-
+    Route::get('admin/category/list', [CategoryController::class, 'list']);
+    Route::get('admin/category/add', [CategoryController::class, 'add']);
+    Route::post('admin/category/add', [CategoryController::class, 'insert']);
+    Route::get('admin/category/edit/{id}', [CategoryController::class, 'edit']);
+    Route::post('admin/category/edit/{id}', [CategoryController::class, 'update']);
+    Route::get('admin/category/delete/{id}', [CategoryController::class, 'delete']);
    
 });
 
