922413cfdae466df3bdc61cf6e380b81a90752ca
Maha182 <maaalmashharawi@effat.edu.sa>
2023-11-24 17:28:50 +0300

    update files

--- resources/views/admin/category/list.blade.php
+++ resources/views/admin/category/list.blade.php
@@ -9,10 +9,10 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Admin List</h1>
+            <h1>Category List</h1>
           </div>
           <div class="col-sm-6" style="text-align:right;">
-            <a href="{{url('admin/admin/add')}}" class="btn btn-primary">Add new admin</a>
+            <a href="{{url('admin/category/add')}}" class="btn btn-primary">Add new Category</a>
           </div>
         </div>
       </div><!-- /.container-fluid -->
@@ -37,13 +37,37 @@
                     <tr>
                       <th>#</th>
                       <th>Name</th>
-                      <th>Email</th>
+                      <th>Slug</th>
+                      <th>Meta Title</th>
+                      <th>Meta Describtion</th>
+                      <th>Meta Kyewords</th>
+                      <th>Created By</th>
                       <th>Status</th>
+                      <th>Created Date</th>
                       <th>Action</th>
                     </tr>
                   </thead>
                   <tbody>
-                    
+                    @foreach($getRecord as $value)
+                    <tr>
+                      <td>{{$value->id}}</td>
+                      <td>{{$value->name}}</td>
+                      <td>{{$value->slug}}</td>
+                      <td>{{$value->meta_title}}</td>
+                      <td>{{$value->meta_describtion}}</td>
+                      <td>{{$value->meta_kyewords}}</td>
+                      <td>{{$value->created_by_name}}</td>
+                      <td>{{ ($value->status ==0)? 'Active':'Inactive' }}</td>
+                      <td>{{date('d-m-y', strtotime($value->created_at))}}</td>
+                      <td>
+                      <a href="{{url('admin/category/edit/'.$value->id)}}" class="btn btn-primary">Edit</a>
+                      <a href="{{url('admin/category/delete/'.$value->id)}}" class="btn btn-danger">Delete</a>
+                      
+                      </td>
+                      
+                    </tr>
+                    @endforeach
+
                   </tbody>
                 </table>
               </div>
