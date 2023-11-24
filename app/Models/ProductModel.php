38d1721a58b604b85db10c033758f3ac2aa70073
Maha182 <maaalmashharawi@effat.edu.sa>
2023-11-24 23:13:00 +0300

    create productModel

---
+++ app/Models/ProductModel.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductModel extends Model
+{
+    use HasFactory;
+    protected $table = 'product';
+
+    static public function checkSlug($slug)
+    {
+        return self::where('slug','=',$slug)->count();
+
+    }
+}
