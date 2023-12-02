d0ea2fd47c2b7c88eb8a5d622386caabca4d00e3
Maha182 <maaalmashharawi@effat.edu.sa>
2023-12-02 19:29:18 +0300

    update about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,3 +1,88 @@
 @extends('layouts.app')
 @section('content')
+
+<section id="about" class="padding-xlarge">
+      <div class="container">
+        <div class="row">
+          <div class="offset-md-2 col-md-8" dir="rtl">
+            <span class="title-accent fs-6 text-uppercase" data-aos="fade" data-aos-easing="ease-in" data-aos-duration="1000" data-aos-once="true" >من نحن </span>
+            <h3 class="py-3" data-aos="fade" data-aos-easing="ease-in" data-aos-duration="1500" data-aos-once="true">برونز عباية علامة تجارية سعودية متخصصين لتلبية جميع متطلبات عملائنا كما اننا حريصون على تقديم تصاميم مميزة بجودة عالية مقابل سعر المناسب</h3>
+            <p data-aos="fade" data-aos-easing="ease-in" data-aos-duration="1800" data-aos-once="true"></p>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+    
+    
+    <section id="faqs" class="padding-xlarge">
+      <div class="container">
+        <div class="row">
+          <div class="offset-md-2 col-md-8">
+            <h3 class="text-center mb-5" data-aos="fade" data-aos-easing="ease-in" data-aos-duration="1000" data-aos-once="true">اسئلة شائعة</h3>
+            <div dir="rtl" class="accordion accordion-flush" id="accordionFlush" data-aos="fade" data-aos-easing="ease-in" data-aos-duration="1500" data-aos-once="true">
+              <div class="accordion-item">
+                <h4 class="accordion-header" id="flush-headingOne3">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="true" aria-controls="flush-collapseOne">
+                    مدة الانتهاء من الطلب؟
+
+                  </button>
+                </h4>
+                <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                  <div class="accordion-body">
+                    <p>مدة العمل من 7-14 يوم و قد يستغرق بضعة ايام اضافية في حال الضغط او العروض ( ملاحظة :  لا يحتسب يوم الجمعه من عدد ايام العمل )
+                    </p>
+                  </div>
+                </div>
+              </div>
+      
+              <div class="accordion-item">
+                <h4 class="accordion-header" id="flush-headingTwo3">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="true" aria-controls="flush-collapseTwo">
+                    سياسة برونز عباية للاستبدال و الاسترجاع ؟
+
+                  </button>
+                </h4>
+                <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+                  <div class="accordion-body">
+                    <p>نعتذر لعدم توفر خدمة الاستبدال او الاسترجاع نظرًا لاننا تقوم بتفصيل الطلب حسب رغباتك كما ان العربون لا يسترد بعد رفع الطلب ولكن في حال وجود عيوب مصنعية المتجر متكفل بتكاليف التعديل و التوصيل</p>
+                  </div>
+                </div>
+              </div>
+      
+              <div class="accordion-item">
+                <h4 class="accordion-header" id="flush-headingThree3">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="true" aria-controls="flush-collapseThree">
+                    هل يمكنني اجراء أي تعديل على العباية ؟ 
+                  </button>
+                </h4>
+                <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+                  <div class="accordion-body">
+                    <p>يمكنك اضافة التعديل في خانة الملاحظات قبل ارفاق الطلب فقط</p>
+                  </div>
+                </div>
+              </div>
+      
+              <div class="accordion-item">
+                <h4 class="accordion-header" id="flush-headingFour3">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="true" aria-controls="flush-collapseFour">
+                    ما هي طريقة الغسيل الصحيحة للعباية ؟
+
+                  </button>
+                </h4>
+                <div id="flush-collapseFour" class="accordion-collapse collapse" aria-labelledby="flush-headingFour" data-bs-parent="#accordionFlushExample">
+                  <div class="accordion-body">
+                    <p>يفضل غسيلها يدوياً بشامبو سائل خاص للعبايات
+                      احرصي على عدم سكب الشامبو مباشرةً على العباية و عدم فركها بقوة
+                      حيث ان بعض الاقمشة تستزم غسيلاً جافا منها (العبايات المطرزة بالخرز او الخيط )
+                      </p>
+                  </div>
+                </div>
+              </div>
+      
+            </div>      
+          </div>
+        </div>
+      </div>
+    </section>
 @endsection
