e47489abf153394403c6a7b0a8ac4c8341369736
Walaa M. Oudah <woudah01@gmail.com>
2023-12-07 01:04:44 +0300

    Update
    Order Management Controller

--- app/Http/Controllers/Admin/OrderManagementController.php
+++ app/Http/Controllers/Admin/OrderManagementController.php
@@ -25,40 +25,6 @@ class OrderManagementController extends Controller
     }
     
 
-    public function add()
-    {
-        $data['header_title']= 'Add New Product';
-        return view('admin.product.add',$data);
-    }
-
-    public function insert(Request $request)
-    {
-        $title = trim($request->title);
-        $product = new ProductModel;
-        $product->title=$title;
-        $product->created_by=Auth::user()->id;
-        $product->save();
-
-        $slug = Str::slug($title,"-");
-
-        $checkSlug = ProductModel::checkSlug($slug);
-       if(empty($checkSlug))
-       {
-            $product->slug=$slug;
-            $product->save();
-       }
-       else 
-       {
-        $new_slug= $slug.'-'.$product->id;
-        $product->slug= $new_slug;
-        $product->save();
-       }
-
-       return redirect('admin/product/edit/'.$product->id);
-
-
-    }
-
     public function edit($product_id)
     {
         $product = ProductModel::getSingle($product_id);
