cc784571bf1a6f4b9a8f0fbf27f8f06b6ec802ce
Maha182 <maaalmashharawi@effat.edu.sa>
2023-12-09 12:08:01 +0300

    translate pages

--- resources/views/cart/list.blade.php
+++ resources/views/cart/list.blade.php
@@ -31,152 +31,153 @@
 </style>
 @endsection
 @section('content')
-<main class="main">
-        	<div class="page-header text-center" style="background-image: url('assets/images/page-header-bg.jpg')">
-        		<div class="container">
-        			<h1 class="page-title">Shopping Cart<span>Shop</span></h1>
-        		</div><!-- End .container -->
-        	</div><!-- End .page-header -->
-            <nav aria-label="breadcrumb" class="breadcrumb-nav">
-                <div class="container">
-                    <ol class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="{{url('')}}">Home</a></li>
-                        <li class="breadcrumb-item"><a href="{{ route('shop') }}">Shop</a></li>
-                        <li class="breadcrumb-item active" aria-current="page">Shopping Cart</li>
-                    </ol>
-                </div><!-- End .container -->
-            </nav><!-- End .breadcrumb-nav -->
+<main class="main" dir="rtl" style="text-align: right;">
+    <div class="page-header text-center" style="background-image: url('assets/images/page-header-bg.jpg')">
+        <div class="container">
+            <h1 class="page-title">عربة التسوق<span>المتجر</span></h1>
+        </div><!-- نهاية .container -->
+    </div><!-- نهاية .page-header -->
+    <nav aria-label="breadcrumb" class="breadcrumb-nav">
+        <div class="container">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="{{url('')}}">الرئيسية</a></li>
+                <li class="breadcrumb-item"><a href="{{ route('shop') }}">المتجر</a></li>
+                <li class="breadcrumb-item active" aria-current="page">عربة التسوق</li>
+            </ol>
+        </div><!-- نهاية .container -->
+    </nav><!-- نهاية .breadcrumb-nav -->
 
-            <div class="page-content">
-            	<div class="cart">
-	                <div class="container">
-	                	<div class="row">
-	                		<div class="col-lg-9">
-	                			<table class="table table-cart table-mobile">
-									<thead>
-										<tr>
-											<th>Product</th>
-											<th>Price</th>
-											<th>Quantity</th>
-											<th>Total</th>
-											<th></th>
-										</tr>
-									</thead>
+    <div class="page-content">
+        <div class="cart">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-9">
+                        <table class="table table-cart table-mobile">
+                            <thead>
+                                <tr>
+                                    <th>المنتج</th>
+                                    <th>السعر</th>
+                                    <th>الكمية</th>
+                                    <th>المجموع</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
 
-									<tbody>
-                                    @php
-                                        $totalcost = 0;
-                                        $productTotal = 0;
-                                    @endphp
-                                    @foreach($getRecord as $value)
-										
-										<tr>
-											<td class="product-col">
-												<div class="product">
-													<figure class="product-media">
-														
-														<a href="#">
-													
-															<img src="{{$value->productImage }}" alt="Product image">
-														
-														</a>
-													</figure>
-
-													<h3 class="product-title">
-														<a href="#">{{$value->product_title}}</a>
-													</h3><!-- End .product-title -->
-												</div><!-- End .product -->
-											</td>
-											<td class="price-col">{{ $value->product_price }} SAR</td>
-        										<td class="quantity-col">
-        										    <div class="cart-product-quantity">
-													
-														<input type="number" name="quantity" class="form-control quantity-input" data-id="{{ $value->id }}" value="{{ old('quantity', $value->product_quantity) }}" min="1" max="10" step="1" data-decimals="0" required onchange="updateQuantity(this)">
-
-        										        
-        										    </div><!-- End .cart-product-quantity -->
-        										</td>
+                            <tbody>
+                                @php
+                                    $totalcost = 0;
+                                    $productTotal = 0;
+                                @endphp
+                                @foreach($getRecord as $value)
+                                    
+                                    <tr>
+                                        <td class="product-col">
+                                            <div class="product">
+                                                <figure class="product-media">
+                                                    
+                                                    <a href="#">
                                                 
-											    <td class="total-col" data-item-total="{{ $value->id }}">{{ $productTotal = $value->product_price * $value->product_quantity  }} SAR</td>
+                                                        <img src="{{$value->productImage }}" alt="صورة المنتج">
+                                                    
+                                                    </a>
+                                                </figure>
+
+                                                <h3 class="product-title">
+                                                    <a href="#">{{$value->product_title}}</a>
+                                                </h3><!-- نهاية .product-title -->
+                                            </div><!-- نهاية .product -->
+                                        </td>
+                                        <td class="price-col">{{ $value->product_price }} ريال سعودي</td>
+                                        <td class="quantity-col">
+                                            <div class="cart-product-quantity">
                                                 
-											    <td class="remove-col" id="deleteitem"><a href="{{url('cart/delete/'.$value->id)}}" class="btn-remove"><i class="icon-close"></i></a></td>
-										</tr>
-										@php $totalcost += $productTotal; @endphp
-										
-									@endforeach
-									
-									</tbody>
-								</table><!-- End .table table-wishlist -->
+                                                <input type="number" name="quantity" class="form-control quantity-input" data-id="{{ $value->id }}" value="{{ old('quantity', $value->product_quantity) }}" min="1" max="10" step="1" data-decimals="0" required onchange="updateQuantity(this)">
 
-	                			
-	                		</div><!-- End .col-lg-9 -->
-	                		<aside class="col-lg-3">
-	                			<div class="summary summary-cart">
-	                				<h3 class="summary-title">Cart Total</h3><!-- End .summary-title -->
+                                                
+                                            </div><!-- نهاية .cart-product-quantity -->
+                                        </td>
+                                        
+                                        <td class="total-col" data-item-total="{{ $value->id }}">{{ $productTotal = $value->product_price * $value->product_quantity  }} ريال سعودي</td>
+                                        
+                                        <td class="remove-col" id="deleteitem"><a href="{{url('cart/delete/'.$value->id)}}" class="btn-remove"><i class="icon-close"></i></a></td>
+                                    </tr>
+                                    @php $totalcost += $productTotal; @endphp
+                                    
+                                @endforeach
+                                
+                            </tbody>
+                        </table><!-- نهاية .table table-wishlist -->
 
-	                				<table class="table table-summary">
-	                					<tbody>
-	                						<tr class="summary-subtotal">
-	                							<td>Subtotal:</td>
-	                							<td id="total-cost-display">{{$totalcost}} SAR</td>
-	                						</tr><!-- End .summary-subtotal -->
-	                						<tr class="summary-shipping">
-	                							<td>Shipping:</td>
-	                							<td>&nbsp;</td>
-	                						</tr>
+                        
+                    </div><!-- نهاية .col-lg-9 -->
+                    <aside class="col-lg-3">
+                        <div class="summary summary-cart">
+                            <h3 class="summary-title">إجمالي السلة</h3><!-- نهاية .summary-title -->
 
-	                						<tr class="summary-shipping-row">
-	                							<td>
-													<div class="custom-control custom-radio">
-														<input type="radio" id="free-shipping" name="shipping" class="custom-control-input">
-														<label class="custom-control-label" for="free-shipping">Free Shipping</label>
-													</div><!-- End .custom-control -->
-	                							</td>
-	                							<td>$0.00</td>
-	                						</tr><!-- End .summary-shipping-row -->
+                            <table class="table table-summary">
+                                <tbody>
+                                    <tr class="summary-subtotal">
+                                        <td>المجموع الفرعي:</td>
+                                        <td id="total-cost-display">{{$totalcost}} ريال سعودي</td>
+                                    </tr><!-- نهاية .summary-subtotal -->
+                                    <tr class="summary-shipping">
+                                        <td>الشحن:</td>
+                                        <td>&nbsp;</td>
+                                    </tr>
 
-	                						<tr class="summary-shipping-row">
-	                							<td>
-	                								<div class="custom-control custom-radio">
-														<input type="radio" id="standart-shipping" name="shipping" class="custom-control-input">
-														<label class="custom-control-label" for="standart-shipping">Standart:</label>
-													</div><!-- End .custom-control -->
-	                							</td>
-	                							<td>$10.00</td>
-	                						</tr><!-- End .summary-shipping-row -->
+                                    <tr class="summary-shipping-row">
+                                        <td>
+                                            <div class="custom-control custom-radio">
+                                                <input type="radio" id="free-shipping" name="shipping" class="custom-control-input">
+                                                <label class="custom-control-label" for="free-shipping">شحن مجاني</label>
+                                            </div><!-- نهاية .custom-control -->
+                                        </td>
+                                        <td>$0.00</td>
+                                    </tr><!-- نهاية .summary-shipping-row -->
 
-	                						<tr class="summary-shipping-row">
-	                							<td>
-	                								<div class="custom-control custom-radio">
-														<input type="radio" id="express-shipping" name="shipping" class="custom-control-input">
-														<label class="custom-control-label" for="express-shipping">Express:</label>
-													</div><!-- End .custom-control -->
-	                							</td>
-	                							<td>$20.00</td>
-	                						</tr><!-- End .summary-shipping-row -->
+                                    <tr class="summary-shipping-row">
+                                        <td>
+                                            <div class="custom-control custom-radio">
+                                                <input type="radio" id="standart-shipping" name="shipping" class="custom-control-input">
+                                                <label class="custom-control-label" for="standart-shipping">عادي:</label>
+                                            </div><!-- نهاية .custom-control -->
+                                        </td>
+                                        <td>$10.00</td>
+                                    </tr><!-- نهاية .summary-shipping-row -->
 
-	                						<tr class="summary-shipping-estimate">
-	                							<td>Estimate for Your Country<br> <a href="dashboard.html">Change address</a></td>
-	                							<td>&nbsp;</td>
-	                						</tr><!-- End .summary-shipping-estimate -->
+                                    <tr class="summary-shipping-row">
+                                        <td>
+                                            <div class="custom-control custom-radio">
+                                                <input type="radio" id="express-shipping" name="shipping" class="custom-control-input">
+                                                <label class="custom-control-label" for="express-shipping">سريع:</label>
+                                            </div><!-- نهاية .custom-control -->
+                                        </td>
+                                        <td>$20.00</td>
+                                    </tr><!-- نهاية .summary-shipping-row -->
 
-	                						<tr class="summary-total">
-	                							<td>Total:</td>
-	                							<td id="total-cost">{{$totalcost}} SAR</td>
-	                						</tr><!-- End .summary-total -->
-	                					</tbody>
-	                				</table><!-- End .table table-summary -->
+                                    <tr class="summary-shipping-estimate">
+                                        <td>تقدير لبلدك<br> <a href="dashboard.html">تغيير العنوان</a></td>
+                                        <td>&nbsp;</td>
+                                    </tr><!-- نهاية .summary-shipping-estimate -->
 
-	                				<a href="{{url('checkout/list')}}" class="btn btn-outline-primary-2 btn-order btn-block">PROCEED TO CHECKOUT</a>
-	                			</div><!-- End .summary -->
+                                    <tr class="summary-total">
+                                        <td>المجموع:</td>
+                                        <td id="total-cost">{{$totalcost}} ريال سعودي</td>
+                                    </tr><!-- نهاية .summary-total -->
+                                </tbody>
+                            </table><!-- نهاية .table table-summary -->
 
-		            			<a href="category.html" class="btn btn-outline-dark-2 btn-block mb-3"><span>CONTINUE SHOPPING</span><i class="icon-refresh"></i></a>
-	                		</aside><!-- End .col-lg-3 -->
-	                	</div><!-- End .row -->
-	                </div><!-- End .container -->
-                </div><!-- End .cart -->
-            </div><!-- End .page-content -->
-        </main><!-- End .main -->
+                            <a href="{{url('checkout/list')}}" class="btn btn-outline-primary-2 btn-order btn-block">المتابعة إلى الدفع</a>
+                        </div><!-- نهاية .summary -->
+
+                        <a href="category.html" class="btn btn-outline-dark-2 btn-block mb-3"><span>مواصلة التسوق</span><i class="icon-refresh"></i></a>
+                    </aside><!-- نهاية .col-lg-3 -->
+                </div><!-- نهاية .row -->
+            </div><!-- نهاية .container -->
+        </div><!-- نهاية .cart -->
+    </div><!-- نهاية .page-content -->
+</main><!-- نهاية .main -->
+
 @endsection
 @section('script')
 <script>
--- resources/views/email/order.blade.php
+++ resources/views/email/order.blade.php
@@ -1,54 +1,52 @@
-<!DOCTYPE html >
-<html lang="en">
+<!DOCTYPE html>
+<html lang="ar">
 <head>
 
     <meta charset="UTF-8">
-    <meta name= "viewport" content="width=device-widt, initial-scale-1.0"> 
-    <meta http-equiv= "X-UA-Compatible" content= "ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <title> Order Email </title>
+    <title>بريد تأكيد الطلب</title>
 
 </head>
-<body style="font-family: Arial,Helvetica, sans,serif; font-size:16px;">
-            <h1>thanks for Your order</h1>
-            <h2> Your order id is:#{{$mailData['order']->id}}</h2>
+<body style="font-family: Arial, Helvetica, sans-serif; font-size:16px; direction: rtl; text-align: right;">
+    <h1>شكراً لك على طلبك</h1>
+    <h2>رقم طلبك هو: #{{$mailData['order']->id}}</h2>
 
-            <h2>Shipping Address</h2>
-            <address>
-                <strong>{{ $mailData[ 'user' ]->name}}</strong><br>
+    <h2>عنوان الشحن</h2>
+    <address>
+        <strong>{{ $mailData['user']->name}}</strong><br>
 
-                {{ $mailData['address']->city }}, {{ $mailData['address']->country }} {{ $mailData['address']->street}}<br>
-                Email: {{ $mailData[ 'user' ]->email }}
-            </address>
+        {{ $mailData['address']->city }}, {{ $mailData['address']->country }} {{ $mailData['address']->street}}<br>
+        البريد الإلكتروني: {{ $mailData['user']->email }}
+    </address>
 
-            <h2> product</h2>
+    <h2>المنتج</h2>
 
-            <table cellpadding="3" cellspacing="3" border="0" width="700">
-                <thead>
-                    <tr style="background: #CCC;">
-                         <th>Product</th>
-                         <th>Price</th>
-                         <th>Qty</th>
-                         <th>Total</th>
-
-                    
-                    </tr>
-                </thead>                              
-                <tbody>
-                    @foreach ($mailData['orderitems'] as $item)
-                    <tr>
-                        <td>{{ $item->product_title }}</td>
-                        <td>{{ number_format($item->product_price,2) }}</td>
-                        <td>{{ $item->product_quantity }}</td> 
-                        <td>{{ number_format($item->product_price * $item->product_quantity ,2) }}</td>
-                    </tr>
-                     @endforeach 
-                     <tr>
-                        <th colspan="3" align="right">total:</th>
-                        <td>${{number_format($mailData['order']->totalcost,2)}}
-                     </tr> 
-                </tbody>
-            </table>
+    <table cellpadding="3" cellspacing="3" border="0" width="700">
+        <thead>
+            <tr style="background: #CCC;">
+                <th>المنتج</th>
+                <th>السعر</th>
+                <th>الكمية</th>
+                <th>المجموع</th>
+            </tr>
+        </thead>                              
+        <tbody>
+            @foreach ($mailData['orderitems'] as $item)
+            <tr>
+                <td>{{ $item->product_title }}</td>
+                <td>{{ number_format($item->product_price, 2) }} ريال</td>
+                <td>{{ $item->product_quantity }}</td> 
+                <td>{{ number_format($item->product_price * $item->product_quantity, 2) }} ريال</td>
+            </tr>
+            @endforeach 
+            <tr>
+                <th colspan="3" align="right">المجموع:</th>
+                <td>${{ number_format($mailData['order']->totalcost, 2) }}
+            </tr> 
+        </tbody>
+    </table>
 
 </body> 
 </html>
